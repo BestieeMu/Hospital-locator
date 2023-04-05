caf51e7bc6388eb4386df5d66e1ceece45692949
agba_coder <Chideraigboka7@gmail.com>
2023-04-05 16:19:29 +0000

    no page found handling error fix tst.

---
+++ src/Pages/Error.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Error = () => {
+  return (
+    <>
+      <div className="h-scren w-full flex justify-center items-center">
+        <h1 className="text-5xl font-bold"> Page Not Found </h1>
+      </div>
+    </>
+  );
+};
+
+export default Error;
